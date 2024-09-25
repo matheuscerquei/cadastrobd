09da0475f3967231f4f29a7f53f670c3ec6f81e9
matheuscerquei <103291017+matheuscerquei@users.noreply.github.com>
2024-09-24 21:15:21 -0300

    complementar

---
+++ cadastro.php
@@ -0,0 +1,23 @@
+<?php
+require 'configPDO.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastro</title>
+</head>
+<body>
+<form action="inserir.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required><br><br>
+        
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
---
+++ configPDO.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fdp</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+//criando conexão
+$pdo = new PDO("mysql:dbname=cadastro;host=localhost:3306", "root","cimatec"); 
+
+//teste conecção
+/*if($pdo){
+echo "banco conectado";
+}else{
+    echo "nao conectou";
+}*/
+
+
+
+?>
